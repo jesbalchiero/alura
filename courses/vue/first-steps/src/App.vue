469e90784abcc7f80c1214b2fe60042543be51b7
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2023-11-29 16:08:05 -0300

    add vue projects

---
+++ courses/vue/first-steps/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2>{{ titleWebPage }}</h2>
+    <ul>
+      <li v-for="p in photos">
+        <img :src="p.url" :alt="p.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titleWebPage: 'Curso Vue Alura',
+      photos: [],
+    }
+  },
+  created() {
+    this.$http.get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(p => this.photos = p, err => console.log(err));
+  }
+}
+</script>
+
+<style>
+
+</style>
